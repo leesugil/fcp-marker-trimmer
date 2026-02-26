907a6cff0e697470aed3e39ddef23a563bd2f42f
leesugil <sugilmath@gmail.com>
2026-02-25 19:38:14 -0500

    0.0.2

--- src/fcp_marker_trimmer/cli.py
+++ src/fcp_marker_trimmer/cli.py
@@ -2,7 +2,6 @@
 
 import argparse
 import xml.etree.ElementTree as ET
-from urllib.parse import urlparse, unquote
 
 from fcp_io import fcpxml_io
 
@@ -14,7 +13,7 @@ def main():
     # output
     parser.add_argument("--affix", type=str, default='markers_trimmed_', help="affix to modify the output filename")
     # debug
-    parser.add_argument("--debug", type=str, default=0, help="DEBUG output")
+    parser.add_argument("--debug", action='store_true', help="(experimental) display debug messages.")
 
     args = parser.parse_args()
 
@@ -23,14 +22,12 @@ def main():
     print(f"fcpxml file: {xf}")
     print(f"video file: {vf}")
 
-    DEBUG = True if (args.debug == 1) else False
-
     # <fcpxml>
     tree, root = fcpxml_io.get_fcpxml(xf)
     # '100/6000s'
     fps = fcpxml_io.get_fps(root)
 
-    trim_markers_in_spine(root=root, fps=fps, debug=DEBUG):
+    trim_markers_in_spine(root=root, fps=fps, debug=args.debug):
 
     fcpxml_io.save_with_affix(tree=tree, src_filepath=xf, affix=args.affix)
 
